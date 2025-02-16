561f5f6b6f99ae18bf5612ae41fcfa74068c6eba
Max McCorkel <mccorkel@gmail.com>
2025-02-16 15:33:06 -0600

    Trying to get rid of peer dependencies

--- amplify/functions/moderation-handler/resource.ts
+++ amplify/functions/moderation-handler/resource.ts
@@ -2,11 +2,5 @@ import { defineFunction } from '@aws-amplify/backend';
 
 export const moderationHandler = defineFunction({
   name: 'moderation-handler',
-  entry: './index.ts',
-  bundling: {
-    external: [
-      '@aws-sdk/client-s3',
-      '@aws-sdk/client-rekognition'
-    ]
-  }
+  entry: './index.ts'
 }); 
